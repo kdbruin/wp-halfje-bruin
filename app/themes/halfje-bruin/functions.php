c79d11cd191a92aa7e93c7166f145c2ef34ff9cc
Kees de Bruin <halfje.bruin@gmail.com>
2014-03-24 11:32:57 +0100

    - Use proper SSH user for deployment to server - Updated theme Halfje-Bruin

---
+++ app/themes/halfje-bruin/functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Halfje-Bruin WordPress thema gebaseerd op het Cazuela thema
+ *
+ * @package Cazuela
+ * @subpackage Halfje-Bruin
+ * @author Kees de Bruin
+ */
+
+/**
+ *  Localization
+ */
+add_action('after_setup_theme', 'hb_theme_setup');
+function hb_theme_setup()
+{
+    load_child_theme_textdomain('halfje-bruin', get_stylesheet_directory() . '/languages');
+}
+
