804fdc446506f0497c5485133cd1bdd969bf4566
Kees de Bruin <halfje.bruin@gmail.com>
2014-04-22 12:00:29 +0200

    Underscore-HB theme changes: - minor changes to custom LESS - split post footer from content.php

--- app/themes/underscore-hb/functions.php
+++ app/themes/underscore-hb/functions.php
@@ -97,7 +97,7 @@ function underscore_hb_scripts() {
 
 	wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css', array( 'underscore-hb-style' ), '3.1.1', 'all' );
 
-	wp_register_style( 'theme-custom-css', get_template_directory_uri() . '/css/custom.css', array( 'bootstrap-css' ), '20140416', 'all' );
+	wp_register_style( 'theme-custom-css', get_template_directory_uri() . '/css/custom.min.css', array( 'bootstrap-css' ), '20140416', 'all' );
 
 	wp_enqueue_script( 'bootstrap-js' );
 	wp_enqueue_style( 'bootstrap-css' );
--- app/themes/underscore-hb/header-frontpage.php
+++ app/themes/underscore-hb/header-frontpage.php
@@ -60,6 +60,13 @@
   </div><!-- .container -->
 </nav>
 
+	<div class='container'>
+		<div class="site-branding">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		</div><!-- .site-branding -->
+	</div><!-- .container -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="container">
