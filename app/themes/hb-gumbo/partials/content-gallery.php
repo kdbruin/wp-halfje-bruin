be6bc93d1ece10be4ab97eb0a1ae82198db237ef
Kees de Bruin <halfje.bruin@gmail.com>
2014-07-15 11:31:10 +0200

    Updated Halfje-Bruin Gumbo theme to add support for FooGallery

--- app/themes/hb-gumbo/partials/content-gallery.php
+++ app/themes/hb-gumbo/partials/content-gallery.php
@@ -16,12 +16,27 @@
 			title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'gumbo' ), the_title_attribute( 'echo=0' ) ) ); ?>"
 			rel="bookmark">
 			<?php
-			if ( has_post_thumbnail() ) :
-				the_post_thumbnail( 'thsp-archives-featured', array( 'class' => 'entry-featured' ) );
-			endif; // has_post_thumbnail()
-			?>		
+			$shortcodes = foogallery_extract_gallery_shortcodes(get_the_content());
+			$count = 0;
+
+			if (empty($shortcodes))
+			{
+				if ( has_post_thumbnail() ) the_post_thumbnail( 'thsp-archives-featured', array( 'class' => 'entry-featured' ) );
+				$count = count( get_post_gallery_images( $post ) );
+			}
+			else
+			{
+				$id = array_keys($shortcodes)[0];
+				$fg = FooGallery::get_by_id($id);
+				$feat_id = $fg->find_featured_attachment_id();
+				if ( $feat_id && $image = @wp_get_attachment_image( $feat_id, 'thsp-archives-featured', false, array( 'class' => 'entry-featured' ) ) )
+				{
+					echo $image;
+				}
+				$count = count( $fg->attachments() );
+			}
+			?>
 			
-			<?php $count = count( get_post_gallery_images( $post ) ); ?>
 			<h1 class="entry-title"><?php the_title(); ?> <span>(<?php echo $count; ?> <?php _e( 'images', 'gumbo' ); ?>)</span>
 			</h1>
 		</a>
