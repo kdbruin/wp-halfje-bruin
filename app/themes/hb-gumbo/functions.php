c57f9c697562908eecb0414ac213adbfa8baa632
Kees de Bruin <halfje.bruin@gmail.com>
2014-07-17 10:21:24 +0200

    Halfje-Bruin Gumbo theme: - removed footer

--- app/themes/hb-gumbo/functions.php
+++ app/themes/hb-gumbo/functions.php
@@ -11,17 +11,26 @@
 /**
  * Theme setup
  */
-add_action('after_setup_theme', 'hb_theme_setup');
-
 function hb_theme_setup()
 {
 	/* Localisation */
-	load_child_theme_textdomain('halfje-bruin', get_stylesheet_directory() . '/languages');
+	load_child_theme_textdomain( 'halfje-bruin', get_stylesheet_directory() . '/languages' );
 }
+add_action( 'after_setup_theme', 'hb_theme_setup' );
+
+/**
+* Update hooks and actions
+*/
+function hb_update_actions()
+{
+	remove_action( 'gumbo_credits', 'gumbo_footer_credits', 10 );
+}
+add_action( 'init', 'hb_update_actions' );
 
 /**
  * Other modifications
  */
-require_once ('inc/gallery.php');
-require_once ('inc/series.php');
-require_once ('inc/template-tags.php');
+require_once (get_stylesheet_directory() . '/inc/gallery.php');
+require_once (get_stylesheet_directory() . '/inc/series.php');
+require_once (get_stylesheet_directory() . '/inc/template-tags.php');
+
